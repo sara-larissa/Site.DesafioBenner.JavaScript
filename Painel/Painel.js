e2173cdbb5f0391acfbcd36b854a59c459456bee
SARA L. SOUZA <108760378+sara-larissa@users.noreply.github.com>
2022-11-04 16:11:48 -0300

    Update Painel.js

--- Painel/Painel.js
+++ Painel/Painel.js
@@ -1,4 +1,5 @@
 
+
 import{clientes} from '../clientes.js'
 import{produtos} from '../produtos.js'
 
@@ -16,6 +17,8 @@ for(let cliclado of clicaveis){
             if(formClientes.classList.contains('desativado')) {
                 formClientes.classList.remove('desativado')
                 navegarClientes(1)
+                formProdutos.classList.add('desativado');
+                formPedidos.classList.add('desativado');
             }
             else
                 formClientes.classList.add('desativado')
@@ -26,6 +29,8 @@ for(let cliclado of clicaveis){
             if(formProdutos.classList.contains('desativado')) {
             formProdutos.classList.remove('desativado');
             navegarProdutos(1)
+            formClientes.classList.add('desativado');
+            formPedidos.classList.add('desativado');
         }
             else
             formProdutos.classList.add('desativado')  ;
@@ -33,9 +38,11 @@ for(let cliclado of clicaveis){
     
         else if(evento.target.id == 'pedidos')
         {
-            if(formPedidos.classList.contains('desativado'))
+            if(formPedidos.classList.contains('desativado')) {
             formPedidos.classList.remove('desativado');
-
+            formClientes.classList.add('desativado');
+            formProdutos.classList.add('desativado');
+        }
             else
             formPedidos.classList.add('desativado');
         }
@@ -181,16 +188,44 @@ idCliente.addEventListener('focusout', function(){
     document.getElementById('nomeCliente').value = clientes[index]['nomeCliente']
 })
 
-// let idProduto = document.getElementById('idProduto')
-// idProduto.addEventListener('focusout', function(){
-//     let index = idProduto.value-1
-//     if(index < 0){
-//         index = 0
-//     }
-//     document.getElementById('codProduto').value = produtos[index]['codprodutos']
-// })
+let codigoProduto = document.getElementById('codigoProduto');
+codigoProduto.addEventListener('focusout', function(){
+    let index = codigoProduto.value -1;
+    if(index < 0){
+       index = 0
+    }
+document.getElementById('descProd').value = produtos[index]['descProduto'] 
+document.getElementById('valorProd').value = produtos[index]['precoProduto']
+})
+
+const adcnar = document.getElementById('adcnar');
+adcnar.addEventListener('click', function(){
+    listaTabela();
+})
 
 
+function listaTabela() {
+    let tbody = document.getElementById('tabela');
+    // .push(adc elemento no array e .length a gente sabe quantos elementos tem)
+    // for (let i = 0; i < this.arrayProdutos.length; i++) {
+        let tr = tbody.insertRow(-1);
+        console.log(tr)
+
+        let td_item = tr.insertCell(0);//insere uma nova coluna
+        let td_descricao = tr.insertCell(1);
+        let td_preco = tr.insertCell(2);
+        let td_total = tr.insertCell(3);
+        let td_acoes = tr.insertCell(4);
+        // let tr = tbody.insertRow(tbody.parentElement.rows.length-1)
+
+        td_item.textContent = codigoProduto.value;
+        td_descricao.textContent = descProd.value;
+        td_preco.textContent = valorProd.value;
+        td_total.textContent = qntProd.value;
+        td_acoes.textContent = (Number(valorProd.value) * Number(qntProd.value)).toFixed(2);
+        let total = document.getElementById("total");
+        total.textContent = (Number(total.textContent) + Number(td_acoes.textContent)).toFixed(2);
+}
 
 
 
